d1de6da0e5eecf9e52afbed06c2270b5fa208b2a
Sylwia Michalska <sylwiam@student.agh.edu.pl>
2021-11-22 09:34:17 +0100

    wstępne testy

--- servers_tests.py
+++ servers_tests.py
@@ -1,28 +1,42 @@
 import unittest
-from collections import Counter
 
-from servers import ListServer, Product, Client, MapServer
-
-server_types = (ListServer, MapServer)
+from servers import ListServer, Product, Client, TooManyProductsFoundError
 
 
 class ServerTest(unittest.TestCase):
+    def test_get_entries_returns_sorted_results(self):
+        products = [Product('P12', 1), Product('PP234', 2), Product('PP235', 1)]
+        server = ListServer(products)
+        entries = server.get_entries(1)
+
+        self.assertListEqual([products[0]], entries)
+
+    def test_get_entries_raises_exceptions_if_too_many_results(self):
+        products = [Product('PP234', 2)] * 5
+        server = ListServer(products)
+
+        with self.assertRaises(TooManyProductsFoundError):
+            server.get_entries(2)
 
     def test_get_entries_returns_proper_entries(self):
         products = [Product('P12', 1), Product('PP234', 2), Product('PP235', 1)]
-        for server_type in server_types:
-            server = server_type(products)
-            entries = server.get_entries(2)
-            self.assertEqual(Counter([products[2], products[1]]), Counter(entries))
+        server = ListServer(products)
+        entries = server.get_entries(2)
+        self.assertEqual([products[2], products[1]], entries)
 
 
-class ClientTest(unittest.TestCase):
+class TestClient(unittest.TestCase):
+    def test_total_price_is_zero_if_exception_raised(self):
+        products = [Product('PP234', 2)] * 5
+        server = ListServer(products)
+        client = Client(server)
+        self.assertEqual(None, client.get_total_price(2))
+
     def test_total_price_for_normal_execution(self):
-        products = [Product('PP234', 2), Product('PP235', 3)]
-        for server_type in server_types:
-            server = server_type(products)
-            client = Client(server)
-            self.assertEqual(5, client.get_total_price(2))
+        products = [Product('P234', 2), Product('P235', 3)]
+        server = ListServer(products)
+        client = Client(server)
+        self.assertEqual(5, client.get_total_price(1))
 
 
 if __name__ == '__main__':
